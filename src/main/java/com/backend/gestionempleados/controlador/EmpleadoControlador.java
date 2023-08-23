118dbeb585effa831d1c56a0b8d2c32116107694
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-23 13:45:45 +0200

    Update EmpleadoControlador.java
    @CrossOrigin(origins = "http://localhost:4200/")

--- src/main/java/com/backend/gestionempleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/backend/gestionempleados/controlador/EmpleadoControlador.java
@@ -5,6 +5,7 @@ import com.backend.gestionempleados.modelo.Empleado;
 import com.backend.gestionempleados.repositorio.EmpleadoRepositorio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoControlador {
 
     @Autowired
